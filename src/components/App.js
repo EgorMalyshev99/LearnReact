71b340771f4a588aed18b72ce0143d4ee22e3f39
EgorMalyshev99 <egor.mlshv99@mail.ru>
2020-03-31 16:59:11 +0300

    upd1

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,37 @@
-import React, {PureComponent} from 'react'
-import ArticleList from './ArticleList'
-import articles from '../fixtures'
-import 'bootstrap/dist/css/bootstrap.css'
+import React, { PureComponent } from "react";
+import articles from "../fixtures.js";
+import "bootstrap/dist/css/bootstrap.css";
+import ArticleList from "./ArticleList/Article_list";
 
 class App extends PureComponent {
-    state = {
-        reverted: false
-    }
-    render() {
-        console.log('---', 1)
-        return (
-            <div className="container">
-                <div className="jumbotron">
-                    <h1 className="display-3">
-                        App name
-                        <button className="btn" onClick = {this.revert}>Revert</button>
-                    </h1>
-                </div>
-                <ArticleList articles={this.state.reverted ? articles.slice().reverse() : articles}/>
-            </div>
-        )
-    }
+  state = {
+    reverted: false
+  };
 
-    revert = () => {
-        this.setState({
-            reverted: !this.state.reverted
-        })
-    }
+  render() {
+    console.log("---", 1);
+    return (
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-3">
+            Статьи
+            <button className="btn" onClick={this.revert}>
+              Реверсировать
+            </button>
+          </h1>
+        </div>
+        <ArticleList
+          articles={this.state.reverted ? articles.slice().reverse() : articles}
+        />
+      </div>
+    );
+  }
+
+  revert = () => {
+    this.setState({
+      reverted: !this.state.reverted
+    });
+  };
 }
 
-export default App
+export default App;
